79dc26fdef6f621074923d87f710bf2298d87d68
SandyL88 <SANDY66888866@OUTLOOK.COM>
2024-03-18 01:58:09 -0400

    Initial commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Now I know I don't need to write down<html> in .js file");
+
+function My_First_Function(){
+		var string = "Kiss me, I'm Irish!";
+		var result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+	}
